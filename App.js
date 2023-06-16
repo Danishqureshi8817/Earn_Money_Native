4c8d88c08734197afd0089f857965595fc9fcd27
Danish Qureshi <danishqureshi8817@gmail.com>
2023-06-16 11:36:11 +0530

    notification add in app

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import { View, Text,SafeAreaView } from 'react-native'
-import React,{useState,useLayoutEffect} from 'react'
+import { View, Text,SafeAreaView,Alert } from 'react-native'
+import React,{useState,useLayoutEffect,useEffect} from 'react'
 
 import Navigation from './src/Navigation/index'
 
 import NetInfo,{useNetInfo} from "@react-native-community/netinfo";
 import CheckInternet from './src/screens/CheckInternet/index';
 
+
+
+import {requestUserPermission,notificationListener} from './src/utiles/notificationService'
+
 const App = () => {
 
 const net = useNetInfo();
+
+
+
+
+useEffect(() => {
+  
+  requestUserPermission()
+  notificationListener()
+  
+}, [])
+
 
  
   
